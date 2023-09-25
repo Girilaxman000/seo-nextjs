37484e67331c0be08b12f8ae216dfb758aff25bf
Laxman Giri <kesharigiri000@gmail.com>
2023-09-25 15:03:30 +0545

    using templates in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Home page",
+    template: `%s | seo-app`,
+  },
+  description: "This is home page",
+};
 
 export default function RootLayout({
   children,
